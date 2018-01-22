10ddfc18413194d1ec5c327e4154bde05c11665c
Cristian Petroaca <cristian.petroaca@gmail.com>
2018-01-22 16:11:10 +0200

    Cache neural_net

--- app.py
+++ app.py
@@ -4,21 +4,15 @@ from flask import jsonify
 from flask import g
 from test import Tester
 app = Flask(__name__)
-
-def get_neural_net():
-    if not hasattr(g, 'neural_net'):
-        g.neural_net = Tester()
-    
-    return g.neural_net
+neural_net = Tester()
 
 @app.route("/similarity")
-def calculate_similarity():
+def compute_similarity():
     text1 = request.args.get('text1');
     text2 = request.args.get('text2');
-    tester = get_neural_net()
-    similarity = tester.compute_similarity(text1, text2)
+    similarity = neural_net.compute_similarity(text1, text2)
     
     return jsonify(similarity.item(0))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
